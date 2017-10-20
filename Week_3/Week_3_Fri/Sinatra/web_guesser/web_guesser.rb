d9c3b748883f456e327664d42b89ddc22632baad
alexauff <alexandra.auffray@gmail.com>
2017-10-20 11:01:56 +0200

    I2: uses an ERB template and HTML

--- Week_3/Week_3_Fri/Sinatra/web_guesser/web_guesser.rb
+++ Week_3/Week_3_Fri/Sinatra/web_guesser/web_guesser.rb
@@ -1,7 +1,10 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+#number = rand(100)
+
 get '/' do
-  "The secret number is #{rand(100)}"
+erb :index, :locals => {:number => number}
+#"The secret number is #{number}!"
 end
 
